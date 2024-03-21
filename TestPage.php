e9fbe94da3a9712d37b8445bb938c59982bae466
Teallapesca <133207041+Teallapesca@users.noreply.github.com>
2024-03-21 15:08:20 +0100

    aggiunto popola tabella e sistemato test page (più o meno)

--- TestPage.php
+++ TestPage.php
@@ -4,27 +4,35 @@
 	error_reporting(E_ALL);
 	include 'connessione.php';
 	mysqli_begin_transaction($conn);
-
-	if(isset($_POST["crea_test"])) {
-        $test_title = $_POST["test_title"];
-        $mail = $_SESSION['mail'];
     
+	if(isset($_POST["crea_test"])) {
+        $mail = $_SESSION['mail'];
+        $test_title=$_POST['test_title'];
+        $visualizza=0;
+        if(isset($_POST['visualizza'])){
+            $visualizza=1;
+        }
+    
+
+        echo "dati: ".$mail." ".$test_title." ".$visualizza;
         // Controlla se il test esiste già nel database
         $query = "SELECT * FROM test WHERE Titolo = '$test_title'";
         $result = mysqli_query($conn, $query);
-    
+        echo "tutto ok";
         if(mysqli_num_rows($result) > 0) {
             // Test già presente nel database
             echo "Test già presente nel database.";
         } else {
             // Esegui la query per inserire il titolo del test nel database
-            $sql = "INSERT INTO test (Titolo, mail) VALUES ('$test_title', '$mail')";
-            if (mysqli_query($conn, $sql)) {
-                // Test creato con successo
-                echo "Test creato.";
-            } else {
+            $sql = "CALL CreaTest('$test_title', 0, '$visualizza', '$mail')";
+            //$sql="INSERT INTO test (Titolo, DataTest, Foto, VisualizzaRisposte, MailDocente) VALUES ('$mail', 0, '$visualizza', '$mail');"
+            $risultato=mysqli_query($conn, $sql);
+            if ($risultato === false) {
                 // Errore durante la creazione del test
                 echo "Errore durante la creazione del test: " . mysqli_error($conn);
+                
+            } else {
+                echo "Test creato.";
             }
         }
     }
@@ -49,17 +57,15 @@
 
     <h1>Creazione di un Nuovo Test</h1>
     <form action="TestPage.php" method="post">
-        <label for="test_title">Titolo del Test:</label>
-        <input type="text" id="test_title" name="test_title">
+        Titolo del Test:<br><br>
+        <input type="text" name="test_title">
+        <br><br> <input type=checkbox name=visualizza> Visualizzazione delle risposte
         <button type="submit" name="crea_test">Crea Test</button>
     </form>
-
+    
     <h2>Aggiungi un nuovo Quesito</h2>
-    <form action="TestPage.php" method="post">
-        <label for="question">Quesito:</label><br>
-        <textarea id="question" name="question"></textarea><br><br>
-        <button type="submit" name="crea_quesito">Aggiungi Quesito</button>
-    </form>
+    <a href="CreaQuesito.php"><button type="button">Aggiungi Quesito</button></a>
+    
 
 </body>
 
