5d266fa9b442be930038b3f645d2964bcd44465a
annacampagna <128522689+annacampagna@users.noreply.github.com>
2024-05-07 14:23:32 +0200

    Update TestPage.php

--- TestPage.php
+++ TestPage.php
@@ -9,6 +9,71 @@
             margin-bottom: 20px; /* Aggiunge spazio sotto il modulo */
         }
     </style>
+    <style>
+                .messaggi-dropdown {
+                    position: fixed;
+                    top: 20px;
+                    right: 20px;
+                }
+                .messaggi-icon {
+                    background: none;
+                    border: none;
+                    cursor: pointer;
+                }
+                .messaggi-content {
+                    display: none;
+                    position: absolute;
+                    top: 30px;
+                    right: 0;
+                    width: 200px;
+                    background-color: #f9f9f9;
+                    border: 1px solid #ddd;
+                    padding: 10px;
+                    z-index: 1;
+                }
+                .messaggi-scrivi {
+                    position: fixed;
+                    top: 20px;
+                    right: 160px;
+                }
+                .messaggi-icon {
+                    background: none;
+                    border: none;
+                    cursor: pointer;
+                }
+                .messaggi-form {
+                    display: none;
+                    position: absolute;
+                    top: 30px;
+                    right: 60;
+                    background-color: #f9f9f9;
+                    border: 1px solid #ddd;
+                    padding: 10px;
+                    z-index: 1;
+                }
+            </style>
+            
+<script>
+    function leggiMessaggi() {
+        var messaggiContent = document.getElementById("messaggiContent");
+        if (messaggiContent.style.display === "block") {
+            messaggiContent.style.display = "none";
+        } else {
+            messaggiContent.style.display = "block";
+            messaggiForm.style.display = "none";
+        }
+    }
+
+    function scriviMessaggi() {
+        var messaggiForm = document.getElementById("messaggiForm");
+        if (messaggiForm.style.display === "block") {
+            messaggiForm.style.display = "none";
+        } else {
+            messaggiForm.style.display = "block";
+            messaggiContent.style.display = "none";
+        }
+    }
+</script>
 </head>
 
 <body>
@@ -17,8 +82,21 @@
     <form action="TestPage.php" method="GET">
         Titolo del Test:<br><br>
         <input type="text" name="test_title">
+        <br><br> <input type=checkbox name=visualizza> Visualizzazione delle risposte <br><br>
+        <label for="img">Scegli immagine:</label>
+        <input type="text" id="testImg" name="testImg"><br>
         <button type="submit" name="crea_test">Crea Test</button><br><br><br>
-    </form>
+        </form>
+        <h2>Inserisci i quesiti</h2>        
+        <br>
+        <a href="CreaQuesito.php"><button type="button" name=quesito>Aggiungi Quesito</button></a>
+  
+
+    
+    <a href=hpDocente.php><h2><-</h2></a>
+</body>
+
+</html>
 
 <?php
 // Connessione al database
@@ -30,59 +108,39 @@
     
 	if(isset($_GET["crea_test"])) {
         $mail = $_SESSION['mailDocente'];
-        if($_GET['test_title']!=""){
-            $_SESSION['test_title']=$_GET['test_title'];
-            $test_title=$_SESSION['test_title'];
-            $visualizza=0;
-            $foto=0;
-
-            //echo "dati: ".$mail." ".$test_title." ".$visualizza;
-            // Controlla se il test esiste già nel database
-            $query = "SELECT * FROM test WHERE Titolo = '$test_title'";
-            $result = mysqli_query($conn, $query);
-
-            
-            if(mysqli_num_rows($result) > 0) {
-                // Test già presente nel database
-                echo "Test già presente nel database.";
-            }else {
-                // Esegui la query per inserire il titolo del test nel database
-                $sql = "CALL CreaTest('$test_title', '$foto', '$visualizza', '$mail')";
-                //$sql='INSERT INTO test (Titolo, Foto, VisualizzaRisposte, MailDocente) VALUES ("'.$test_title.'"," '. $foto .'"," '. $visualizza .'"," '.$mailValue.'");';
-
-
-                /*$stmt = $conn->prepare("INSERT INTO test (Titolo, Foto, VisualizzaRisposte, MailDocente) VALUES (?, ?, ?, ?)");
-                $stmt->bind_param("siis", $test_title, $foto_value, $visualizza_value, $mailValue);
-                $foto_value = 0; // Assumendo che Foto sia di tipo INT
-                $visualizza_value = 0;
-                $stmt->execute();
-                if ($stmt->affected_rows === -1) {
-                    // Errore durante l'esecuzione dell'istruzione preparata
-                    echo "Errore durante la creazione del test: " . $stmt->error;
-                } else {
-                    echo "Test creato.";
-                }*/
-
-
-
-                $risultato=mysqli_query($conn, $sql);
-                if ($risultato === false) {
-                    // Errore durante la creazione del test
-                    echo "Errore durante la creazione del test: " . mysqli_error($conn);
-                    
-                } else {
-                    echo "<h2>Inserisci i quesiti</h2>        
-                            <br>
-                        <a href='CreaQuesito.php'><button type='button' name=quesito>Aggiungi Quesito</button></a>
-                    
-                    ";
-                }
-            }
-        }else{
-            echo die("Inserisci il titolo del test <br><br> 
-                    <a href=hpDocente.php><h2><-</h2></a>");
+        $_SESSION['test_title']=$_GET['test_title'];
+        $test_title=$_SESSION['test_title'];
+        $visualizza=0;
+        if(isset($_GET['visualizza'])){
+            $visualizza=1;
         }
         
+        $_SESSION['testImg'] = $_GET['testImg'];
+        $testImg = $_SESSION['testImg'];
+        $testImg = addslashes($testImg);
+        
+
+        //echo "dati: ".$mail." ".$test_title." ".$visualizza;
+        // Controlla se il test esiste già nel database
+        $query = "SELECT * FROM test WHERE Titolo = '$test_title'";
+        $result = mysqli_query($conn, $query);
+
+        if(mysqli_num_rows($result) > 0) {
+            // Test già presente nel database
+            echo "Test già presente nel database.";
+        }else {
+            // Esegui la query per inserire il titolo del test nel database
+            $sql = "CALL CreaTest('$test_title', '$testImg', '$visualizza', '$mail')";
+            
+            $risultato=mysqli_query($conn, $sql);
+            if ($risultato === false) {
+                // Errore durante la creazione del test
+                echo "Errore durante la creazione del test: " . mysqli_error($conn);
+                
+            } else {
+                echo "Test creato.";
+            }
+        }
     }
 
     if (!mysqli_commit($conn)) {
@@ -92,10 +150,4 @@
 
     mysqli_close($conn);
 ?>
-
-    
-<a href=hpDocente.php><h2><-</h2></a>
-</body>
-
-</html>
 
